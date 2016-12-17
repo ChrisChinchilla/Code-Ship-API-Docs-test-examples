e64dc43db40942be6fb392a87fa1b018b0eeee37
Chris Ward <chriswhward@gmail.com>
2016-12-17 18:22:21 +0100

    First commit

---
+++ hooks.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('dotenv').config();
+
+var hooks = require('hooks');
+var md5 = require('md5');
+
+hooks.beforeEach(function (transaction) {
+  var publicKey = process.env.MARVEL_PUBLIC_KEY;
+  var privateKey = process.env.MARVEL_PRIVATE_KEY;
+  console.log(publicKey + privateKey);
+  var ts = String(new Date().getTime());
+  var hash = md5(ts+privateKey+publicKey);
+
+  transaction.fullPath = transaction.fullPath + '?ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash ;
+});
