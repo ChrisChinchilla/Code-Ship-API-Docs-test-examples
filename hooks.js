8b30a15dff7b1c2f72cbef455af6ce68ee24bce8
Chris Ward <chriswhward@gmail.com>
2016-12-18 11:18:41 +0100

    Remove log

--- hooks.js
+++ hooks.js
@@ -8,7 +8,6 @@ var md5 = require('md5');
 hooks.beforeEach(function (transaction) {
   var publicKey = process.env.MARVEL_PUBLIC_KEY;
   var privateKey = process.env.MARVEL_PRIVATE_KEY;
-  console.log(publicKey + privateKey);
   var ts = String(new Date().getTime());
   var hash = md5(ts+privateKey+publicKey);
 
